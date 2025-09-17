c4c52c7696b59b14e479207b2fd8e8aaada45f9d
Chau Cao <averagechau@gmail.com>
2025-09-17 13:08:17 +0700

    feat: add pet marker with Tim avatar on map

--- lib/features/pet_tracking/views/pages/map_page.dart
+++ lib/features/pet_tracking/views/pages/map_page.dart
@@ -2,29 +2,117 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
+import '../widgets/cat_marker_widget.dart';
+
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FlutterMap(
-          options: MapOptions(
-            initialCenter: const LatLng(
-              40.416775,
-              -3.703790,
-            ), // San Francisco coordinates
-            initialZoom: 15.0,
-          ),
-          children: [
-            TileLayer(
-              urlTemplate:
-                  'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
-              subdomains: const ['a', 'b', 'c', 'd'],
-              userAgentPackageName: 'com.chauapps.petsafezone',
+      appBar: AppBar(
+        title: const Text('🐾 Tim Safe Zone Tracker'),
+        centerTitle: true,
+      ),
+      body: Stack(
+        children: [
+          FlutterMap(
+            options: MapOptions(
+              initialCenter: const LatLng(
+                40.416775,
+                -3.703790,
+              ), // San Francisco coordinates
+              initialZoom: 15.0,
             ),
-          ],
+            children: [
+              TileLayer(
+                urlTemplate:
+                    'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+                subdomains: const ['a', 'b', 'c', 'd'],
+                userAgentPackageName: 'com.chauapps.petsafezone',
+              ),
+              MarkerLayer(
+                markers: [
+                  const Marker(
+                    width: 100,
+                    height: 100,
+                    point: LatLng(40.416775, -3.703790),
+                    child: CatMarkerWidget(
+                      imagePath: 'assets/images/tim.jpeg',
+                      markerColor: Colors.blue,
+                      width: 100,
+                      height: 100,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Positioned(
+            top: 16,
+            left: 16,
+            right: 16,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.contrast),
+                  label: const Text('High Contrast'),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 8,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.volume_up),
+                  label: const Text('Announce'),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 8,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.help_outline),
+                  label: const Text('Help'),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 8,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.home),
+                label: const Text('Tim Location'),
+              ),
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.keyboard),
+                label: const Text('Keyboard Guide'),
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/features/pet_tracking/views/widgets/cat_marker_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class CatMarkerWidget extends StatelessWidget {
+  final double width;
+  final double height;
+  final String imagePath;
+  final Color markerColor;
+
+  const CatMarkerWidget({
+    super.key,
+    this.width = 80,
+    this.height = 80,
+    required this.imagePath,
+    this.markerColor = Colors.blue,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Container(
+            width: width * 0.8,
+            height: width * 0.8,
+            margin: EdgeInsets.only(top: 5),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              border: Border.all(color: Colors.white, width: 2),
+              image: DecorationImage(
+                image: AssetImage(imagePath),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SvgPicture.asset(
+            'assets/images/map_marker_circle.svg',
+            width: width,
+            height: height,
+            colorFilter: ColorFilter.mode(markerColor, BlendMode.srcIn),
+          ),
+        ],
+      ),
+    );
+  }
+}
